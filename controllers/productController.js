8da2781d6cdc89a4b3a4eee39f32ede93ed45d9d
Radhika Bansal <radhikajanuary20@gmail.com>
2024-03-19 22:29:59 +0530

    minor changes

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,12 +3,12 @@ const {StatusCodes} = require('http-status-codes');
 
 const createProduct = async(req, res) => {
     const product = await Products.create(req.body);
-    res.status(StatusCodes.CREATED).json({product});
+    return res.status(StatusCodes.CREATED).json({product});
 }
 
 const getAllProducts = async(req, res) => {
     const products = await Products.find({});
-    res.status(StatusCodes.OK).json({products});
+    return res.status(StatusCodes.OK).json({products});
 }
 
 module.exports = {
