b4b2e3514227bdfd544693fbfc37cfafbe102ec7
Radhika Bansal <radhikajanuary20@gmail.com>
2024-03-19 22:12:40 +0530

    error handling for uploading image

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,13 +1,34 @@
-const {StatusCodes} = require('http-status-codes');
-const path = require('path');
+const { StatusCodes } = require("http-status-codes");
+const path = require("path");
+const CustomError = require("../errors");
 
-const uploadProductImage = async(req, res) => {
-    let productImage = req.files.image;
-    const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
-    await productImage.mv(imagePath);
-    return res.status(StatusCodes.OK).json({image: {src: `/uploads/${productImage.name}`}});
-}
+const uploadProductImage = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file uploaded");
+  }
+  let productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload an image");
+  }
+
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please upload image smaller than 1MB"
+    );
+  }
+  
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
 
 module.exports = {
-    uploadProductImage
-}
+  uploadProductImage,
+};
